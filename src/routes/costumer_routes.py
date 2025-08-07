acc3674ace2e980b5963e9ac52aadeda75e24d8f
Gustavo <gustavopfp.trab@gmail.com>
2025-08-07 14:30:46 -0300

    feat: tracos services to interect with mongoDB and costumer routes to send the files to the client

--- src/routes/costumer_routes.py
+++ src/routes/costumer_routes.py
@@ -1,24 +1,29 @@
+from datetime import datetime
 import os
 import json
 import logging
-from typing import List
+from typing import List, Union
 import asyncio
-from src.schemas.customer_schema import CustomerSystemWorkorderSchema  # Changed this line
-from src.CONSTS import PROJECT_ROOT
+from schemas.customer_schema import CustomerSystemWorkorderSchema # Changed this line
+from CONSTS import PROJECT_ROOT
 from pydantic import ValidationError
 logger = logging.getLogger(__name__)
-
-
+DATA_INBOUND_DIR = os.getenv("DATA_INBOUND_DIR")
+DATA_OUTBOUND_DIR = os.getenv("DATA_OUTBOUND_DIR")
+from services.tracos_service import TracOsService
+ 
 class CostumerERPRoute:
     def __init__(self):
-        self.client_get_url =  "./" + "data/inbound"
-        self.client_post_url = str(PROJECT_ROOT) + "/" + "data/outbound"
+        self.client_get_url = DATA_INBOUND_DIR 
+        self.client_post_url = str(DATA_OUTBOUND_DIR)
         self.IOHelper = IOHelper()
+        self.tracos_service = TracOsService()
 
     async def get_costumer_workorder_by_order_number(
         self, orderNo: int
     ) -> CustomerSystemWorkorderSchema:
         """Fetch a workorder from the customer system."""
+        logger.info(f"Getting the workorder {orderNo} from Client's ERP.")
         full_path = os.path.join(self.client_get_url, f"{orderNo}.json")
         json_file = await self.IOHelper.read_json(full_path)
         if json_file is None:
@@ -29,6 +34,29 @@ class CostumerERPRoute:
     async def get_costumer_workorders() -> List[CustomerSystemWorkorderSchema]:
         """Fetch all workorders from the customer system."""
         pass
+
+    async def post_costumer_workorder(
+        self, workorder: CustomerSystemWorkorderSchema
+    ) -> dict: 
+        full_path = os.path.join(self.client_post_url, f"{workorder.orderNo}.json")
+
+        if workorder is None:
+            logger.error(f"Workorder with orderNo {workorder['orderNo']} not found.")
+            return None
+        try:
+            logger.info("Inserting json file in Client's ERP...")
+            await self.IOHelper.write_json(
+                file_path=full_path,
+                data=workorder
+            )
+            await self.tracos_service.update_workorder(
+                number=workorder.orderNo
+            )
+            return workorder.model_dump(mode="json")
+        except Exception as e:
+            logger.error(f"Error inserting json file: {str(e)}")
+            return None
+
 
 
 class IOHelper:
@@ -64,15 +92,56 @@ class IOHelper:
             return None
         except ValidationError as e:
             logger.error(f"Validation error in {file_path}: {str(e)}")
-
+    
     @staticmethod
-    async def write_json(
-        file_path: str, data: List[CustomerSystemWorkorderSchema]
-    ) -> None:
+    def convert_to_payload_json(
+        data: Union[CustomerSystemWorkorderSchema, List[CustomerSystemWorkorderSchema]]
+    ) -> Union[dict, list]:
+        if isinstance(data, list):
+            return [item.model_dump(mode="json") for item in data]
+        return data.model_dump(mode="json")
+    
+    async def write_json(self, 
+        file_path: str, data: List[CustomerSystemWorkorderSchema] | CustomerSystemWorkorderSchema
+    ) -> bool:
         """Write JSON data to a file."""
-        pass
+        payload = self.convert_to_payload_json(data)
+        try:
+            with open(file_path, "w", encoding="utf-8") as json_file:
+                json.dump(payload, json_file, ensure_ascii=False, indent=4)
+                logging.info("Workorder inserted in Client's ERP")
+                return True
+        except FileNotFoundError:
+            logger.error(f"File not found: {file_path}")
+            return None
+        except json.JSONDecodeError as e:
+            logger.error(f"Invalid JSON in {file_path}: {str(e)}")
+            return None
+        except Exception as e:
+            logger.error(f"Error reading {file_path}: {str(e)}")
+            return None
+        except ValidationError as e:
+            logger.error(f"Validation error in {file_path}: {str(e)}")
 
 
 if __name__ == "__main__":
     route = CostumerERPRoute()
-    asyncio.run(route.get_costumer_workorder_by_order_number(orderNo=2))
+    # asyncio.run(route.get_costumer_workorder_by_order_number(orderNo=2))
+    asyncio.run(
+        route.post_costumer_workorder(
+            workorder=CustomerSystemWorkorderSchema(
+                orderNo=123,
+                isActive=True,
+                isCanceled=False,
+                isDeleted=False,
+                isDone=False,
+                isOnHold=False,
+                isPending=False,
+                isSynced=False,
+                summary="Example workorder",
+                creationDate=datetime.now(),
+                lastUpdateDate=datetime.now(),
+            
+            )
+        )
+    )
--- src/services/tracos_service.py
+++ src/services/tracos_service.py
@@ -1,14 +1,21 @@
+from datetime import timezone, datetime
+from types import CoroutineType
+from typing import Any
 from schemas.tracos_schema import TracOSWorkorderSchema
 from motor.motor_asyncio import AsyncIOMotorClient
 import logging
 import os
 from bson.objectid import ObjectId
+from pydantic import ValidationError
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 MONGO_DATABASE = os.getenv("MONGO_DATABASE", "tractian")
 MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "workorders")
 
 logger = logging.getLogger(__name__)
 
+# TODO: Add one method update the workorder to isSynced = true and set a  timestamp
+# TODO: Add a method to insert many workorders
+# TODO: Add a method to get all workorderstimestamp
 class TracOsService:
     """Service to handle operations related to TracOs."""
     def __init__(self):
@@ -29,9 +36,38 @@ class TracOsService:
             logger.info(f"Workorder number {workorder.number} inserted with id {inserted_document.inserted_id}")
             print(f"Workorder number {workorder.number} inserted successfully.")
             return workorder
+        except ValidationError as e:
+            logger.error(f"The workorder you're trying to insert is not valid: {e}")
+            return None
         except Exception as e:
             logger.error(f"Error inserting workorder: {e}")
             return None
+        else:
+            logger.error(f"Error inserting workorder: {e}")
+            return None
+    # TODO: Improve typing here
+    async def update_workorder(self, number: int) -> None:
+        try:
+            logger.info(f"Updating workorder number: {number}...")
+            await self.collection.update_one(
+                filter={
+                    "number" : number
+                },
+                update={
+                    "$set": {
+                        "isSynced": True,
+                        "syncedAt": datetime.now(timezone.utc).isoformat().replace('+00:00', 'Z')
+                    }
+                }          
+            )
+            logger.info("Workorder number {number} updated successfully.")
+            
+          
+        except Exception as e:
+            logger.error(f"Error updating workorder: {e}")
+            return None
+            
+        
         
 if __name__ == "__main__":
     from datetime import datetime
@@ -47,7 +83,15 @@ if __name__ == "__main__":
         createdAt=datetime.now(),
         updatedAt=datetime.now(),
         deleted=False,
-       
+        isSynced=False
     )
-    asyncio.run(tracos_service.insert_workorder(sample_workorder))
+    # asyncio.run(tracos_service.insert_workorder(sample_workorder))
+    asyncio.run(tracos_service.update_workorder(
+        1
+    ))
+
+
+
+
+
 
